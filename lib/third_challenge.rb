852b944643dc3fc843038c950ecef60eb32fd7e0
Tomas Tobar <codetombomb@gmail.com>
2019-12-02 15:03:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,11 +3,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch =>
-      {:name => "Lord Montague",
-        :age => "53"},
+      {name: "Lord Montague",
+        age: "53"},
       :matriarch => 
-      {:name => "Lady Montague",
-        :age => "54"},
+       {name: "Lady Montague",
+        age: "54"},
       :hero => 
       {:name => "Romeo",
         :age => "15"},
