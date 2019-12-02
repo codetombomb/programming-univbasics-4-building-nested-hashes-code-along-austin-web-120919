00f0d48020f470ca07ffa449943f0945d0909054
Tomas Tobar <codetombomb@gmail.com>
2019-12-02 14:59:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,28 +8,22 @@ def third_challenge
       :matriarch => 
       {:name => "Lady Montague",
         :age => "54"},
-      :hero => {
-        :name => "Romeo",
+      :hero => 
+      {:name => "Romeo",
         :age => "15"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-        #your key/value pairs here
-        },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-        #your key/value pairs here
-        },
-      :heroine => {
-        :name => "Juliet",
+      :patriarch => 
+      {:name => "Lord Capulet",
+        :age => "50"},
+      :matriarch => 
+      {:name => "Lady Capulet",
+        :age => "51"},
+      :heroine =>
+      {:name => "Juliet",
         :age => "15",
-        :status => "alive"
-        #your key/value pairs here
-        },
+        :status => "alive"},
       :heroine_friends => []
    }
   }
