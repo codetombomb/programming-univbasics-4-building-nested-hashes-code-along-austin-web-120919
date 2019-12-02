fdf1fe42bc4bce3febbd67fe6c919f84e79a27e6
Tomas Tobar <codetombomb@gmail.com>
2019-12-02 15:03:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,11 @@ def third_challenge
       {name: "Lord Montague",
         age: "53"},
       :matriarch => 
-       {name: "Lady Montague",
+      {name: "Lady Montague",
         age: "54"},
       :hero => 
-      {:name => "Romeo",
-        :age => "15"},
+      {name: "Romeo",
+        age => "15"},
       :hero_friends => []
    }, 
    :capulet => {
