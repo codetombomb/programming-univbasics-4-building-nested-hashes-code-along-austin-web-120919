dd321762782e67f1497c882bdad61b27a664d46d
Tomas Tobar <codetombomb@gmail.com>
2019-12-02 15:05:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,20 +10,20 @@ def third_challenge
         age: "54"},
       :hero => 
       {name: "Romeo",
-        age => "15"},
+        age: "15"},
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => 
-      {:name => "Lord Capulet",
-        :age => "50"},
+      {name: "Lord Capulet",
+        age: "50"},
       :matriarch => 
-      {:name => "Lady Capulet",
-        :age => "51"},
+      {name: "Lady Capulet",
+        age: "51"},
       :heroine =>
-      {:name => "Juliet",
-        :age => "15",
-        :status => "alive"},
+      {name: "Juliet",
+        age: "15",
+        status: "alive"},
       :heroine_friends => []
    }
   }
